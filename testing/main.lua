bfe5d6158b0d667f8d019b8473046f3fbdcfe0b2
zalanwastaken <muditmishra.mr@gmail.com>
2024-09-21 17:17:52 +0530

    snapshort

--- testing/main.lua
+++ testing/main.lua
@@ -1,6 +1,8 @@
 local guified = require("libs/guified")
 function love.load()
     guified.setWindowToBeOnTop()
+    local button = guified.registry.elements.button.new(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, 40, 40, "Hello !")
+    guified.registry.register(button)
 end
 function love.update(dt)
 end
