1f46bedc356e42c5a1f1fe18cf84a67230a2ddee
zalanwastaken <muditmishra.mr@gmail.com>
2024-09-21 19:35:57 +0530

    before publish final

--- testing/main.lua
+++ testing/main.lua
@@ -1,10 +1,20 @@
 local guified = require("libs/guified")
 function love.load()
     guified.setWindowToBeOnTop()
-    local button = guified.registry.elements.button.new(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, 40, 40, "Hello !")
+    button = guified.registry.elements.button.new(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, 40, 40, "Hello !")
+    textbox = guified.registry.elements.textBox.new(0, 0, "GUIFIED VER.INF_DEV")
+    guified.registry.register(textbox)
     guified.registry.register(button)
 end
 function love.update(dt)
+    if button.pressed() then
+        button.text("button was pressed")
+        button.changePos(love.math.random(0, love.graphics.getWidth()), love.math.random(0, love.graphics.getHeight()))
+    end
+    if love.keyboard.isDown("space") then
+        guified.registry.remove(button)
+        guified.registry.remove(textbox)
+    end
 end
 function love.draw()
 end
