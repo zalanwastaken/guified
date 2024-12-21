d7cdb85c0ac6e046eda5fde2ae06bd7eab6762f1
zalanwastaken <muditmishra.mr@gmail.com>
2024-12-21 20:32:43 +0530

    seperated errorhandler

--- testing/libs/guified/init.lua
+++ testing/libs/guified/init.lua
@@ -5,8 +5,10 @@
 local fontsize = 12 --* default font size
 local VK_CAPITAL = 0x14 --* Virtual-Key Code for Caps Lock
 local WARN = true --* Enable warnings ?
+--? requires
 local ffi = require("ffi")
 local OSinterop = require("libs.guified.os_interop") --? contains ffi now
+require("libs.guified.errorhandler") --* setup errorhandler
 --? imp funcs
 ---@return string
 local function getScriptFolder()
@@ -183,7 +185,7 @@ local guified = {
             textInput = { --TODO DELAYED
                 new = function(self, argx, argy, w, h, placeholder, active)
                     warnf("TextInput is a praceholder currently")
-                    error("This element in not implimented yet !")
+                    --error("This element in not implimented yet !")
                     if not(active) then
                         active = false
                     end
@@ -340,20 +342,24 @@ local guified = {
     toggleUpdate = function() --* toggles update
         guifiedlocal.enableupdate = not(guifiedlocal.enableupdate)
     end,
+    ---@return boolean
     getDrawStatus = function() --* returns the draw status
         return(guifiedlocal.enabledraw)
     end,
+    ---@return boolean
     getUpdateStatus = function() --* returns the update status
         return(guifiedlocal.enableupdate)
     end,
+    ---@return table
     getIdTable = function() --* returns the table contaning ids 
         return(guifiedlocal.internalregistry.ids)
     end,
-    getFontSize = function()
+    ---@return number
+    getFontSize = function() --* returns the font size
         return(fontsize)
     end,
     ---@param size number
-    setFontSize = function(size)
+    setFontSize = function(size) --* sets the font size
         fontsize = size
     end
 }
@@ -412,116 +418,6 @@ function love.run()
         end
 	end
 end
---* Error handling
-local utf8 = require("utf8")
-local function error_printer(msg, layer)
-	print((debug.traceback("Error: " .. tostring(msg), 1+(layer or 1)):gsub("\n[^\n]+$", "")))
-end
-function love.errorhandler(msg)
-	msg = tostring(msg)
-	error_printer(msg, 2)
-	if not love.window or not love.graphics or not love.event then
-		return
-	end
-	if not love.graphics.isCreated() or not love.window.isOpen() then
-		local success, status = pcall(love.window.setMode, 800, 600)
-		if not success or not status then
-			return
-		end
-	end
-	-- Reset state.
-	if love.mouse then
-		love.mouse.setVisible(true)
-		love.mouse.setGrabbed(false)
-		love.mouse.setRelativeMode(false)
-		if love.mouse.isCursorSupported() then
-			love.mouse.setCursor()
-		end
-	end
-	if love.joystick then
-		-- Stop all joystick vibrations.
-		for i,v in ipairs(love.joystick.getJoysticks()) do
-			v:setVibration()
-		end
-	end
-	if love.audio then love.audio.stop() end
-	love.graphics.reset()
-	local font = love.graphics.setNewFont(14)
-    local largefont = love.graphics.newFont(44)
-	love.graphics.setColor(1, 1, 1)
-	local trace = debug.traceback()
-	love.graphics.origin()
-	local sanitizedmsg = {}
-	for char in msg:gmatch(utf8.charpattern) do
-		table.insert(sanitizedmsg, char)
-	end
-	sanitizedmsg = table.concat(sanitizedmsg)
-	local err = {}
-	table.insert(err, "Error\n")
-	table.insert(err, sanitizedmsg)
-	if #sanitizedmsg ~= #msg then
-		table.insert(err, "Invalid UTF-8 string in error message.")
-	end
-	table.insert(err, "\n")
-	for l in trace:gmatch("(.-)\n") do
-		if not l:match("boot.lua") then
-			l = l:gsub("stack traceback:", "Traceback\n")
-			table.insert(err, l)
-		end
-	end
-	local p = table.concat(err, "\n")
-	p = p:gsub("\t", "")
-	p = p:gsub("%[string \"(.-)\"%]", "%1")
-	local function draw()
-		if not love.graphics.isActive() then return end
-		love.graphics.clear(0/255, 183/255, 235/255)
-        love.graphics.setFont(largefont)
-        love.graphics.printf("GUIFIED", 0, 44, love.graphics.getWidth(), "center")
-        love.graphics.setFont(font)
-		love.graphics.printf(p, 0, love.graphics.getHeight() / 4, love.graphics.getWidth(), "center")
-		love.graphics.present()
-	end
-	local fullErrorText = p
-	local function copyToClipboard()
-		if not love.system then return end
-		love.system.setClipboardText(fullErrorText)
-		p = p .. "\nCopied to clipboard!"
-	end
-	if love.system then
-		p = p .. "\n\nPress Ctrl+C or tap to copy this error"
-	end
-    local name = love.window.getTitle()
-    if #name == 0 or name == "Untitled" then 
-        love.window.setTitle("Guified error")
-    end
-	return function()
-		love.event.pump()
-		for e, a, b, c in love.event.poll() do
-			if e == "quit" then
-				return 1
-			elseif e == "keypressed" and a == "escape" then
-				return 1
-			elseif e == "keypressed" and a == "c" and love.keyboard.isDown("lctrl", "rctrl") then
-				copyToClipboard()
-			elseif e == "touchpressed" then
-				local buttons = {"OK", "Cancel"}
-				if love.system then
-					buttons[3] = "Copy to clipboard"
-				end
-				local pressed = love.window.showMessageBox("Quit "..name.."?", "", buttons)
-				if pressed == 1 then
-					return 1
-				elseif pressed == 3 then
-					copyToClipboard()
-				end
-			end
-		end
-		draw()
-		if love.timer then
-			love.timer.sleep(0.1)
-		end
-	end
-end
 --* post init
 guifiedlocal.setWindowToBeOnTop = OSinterop(warnf).setWindowToBeOnTop
 --* svc init
@@ -539,3 +435,14 @@ guified.registry.register({
     end
 })
 return(guified)
+--[[
+* Made by Zalanwastaken with LÖVE and some 🎔
+! ________  ________  ___       ________  ________   ___       __   ________  ________  _________  ________  ___  __    _______   ________      
+!|\_____  \|\   __  \|\  \     |\   __  \|\   ___  \|\  \     |\  \|\   __  \|\   ____\|\___   ___\\   __  \|\  \|\  \ |\  ___ \ |\   ___  \    
+! \|___/  /\ \  \|\  \ \  \    \ \  \|\  \ \  \\ \  \ \  \    \ \  \ \  \|\  \ \  \___|\|___ \  \_\ \  \|\  \ \  \/  /|\ \   __/|\ \  \\ \  \   
+!     /  / /\ \   __  \ \  \    \ \   __  \ \  \\ \  \ \  \  __\ \  \ \   __  \ \_____  \   \ \  \ \ \   __  \ \   ___  \ \  \_|/_\ \  \\ \  \  
+!    /  /_/__\ \  \ \  \ \  \____\ \  \ \  \ \  \\ \  \ \  \|\__\_\  \ \  \ \  \|____|\  \   \ \  \ \ \  \ \  \ \  \\ \  \ \  \_|\ \ \  \\ \  \ 
+!   |\________\ \__\ \__\ \_______\ \__\ \__\ \__\\ \__\ \____________\ \__\ \__\____\_\  \   \ \__\ \ \__\ \__\ \__\\ \__\ \_______\ \__\\ \__\
+!    \|_______|\|__|\|__|\|_______|\|__|\|__|\|__| \|__|\|____________|\|__|\|__|\_________\   \|__|  \|__|\|__|\|__| \|__|\|_______|\|__| \|__|
+!                                                                                \|_________|                                                   
+--]]
