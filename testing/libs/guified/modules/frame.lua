a966118760711d6fdc37ae2ba00e2de00627141d
zalanwastaken <muditmishra.mr@gmail.com>
2025-01-22 20:37:10 +0530

    updated frame module

--- testing/libs/guified/modules/frame.lua
+++ testing/libs/guified/modules/frame.lua
@@ -11,10 +11,58 @@ local function createSlider(x, y) --TODO
         end
     })
 end
+local function newFrameObj(x, y, w, h, bgclr, fgclr, title, elements)
+    bgclr = bgclr or {0.5, 0.5, 0.5, 1}
+    fgclr = fgclr or {love.math.random(0, 255) / 100, love.math.random(0, 255) / 100, love.math.random(0, 255) / 100, 1}
+    title = title or "Frame"
+    local grabbeddata = { grabbed = false, x = 0, y = 0 }
+
+    return {
+        name = "Frame SVC",
+        draw = function()
+            --* background
+            love.graphics.setColor(bgclr)
+            love.graphics.rectangle("fill", x, y, w, h)
+
+            --* top bar
+            love.graphics.setColor(fgclr)
+            love.graphics.rectangle("fill", x, y, w, 15)
+            love.graphics.setColor(0, 0, 0)
+            love.graphics.print(title, x + 5, y + 2)
+            love.graphics.setColor(bgclr)
+            love.graphics.rectangle("fill", x+w-15, y+2.5, 10, 10)
+        end,
+
+        update = function()
+            local mouseX, mouseY = love.mouse.getPosition()
+            if love.mouse.isDown(1) then
+                if not grabbeddata.grabbed and mouseX > x and mouseX < x + w and mouseY > y and mouseY < y + 15 then
+                    grabbeddata.grabbed = true
+                    grabbeddata.x = mouseX
+                    grabbeddata.y = mouseY
+                end
+            else
+                grabbeddata.grabbed = false
+            end
+            
+            if grabbeddata.grabbed then
+                local xdiff, ydiff = mouseX - grabbeddata.x, mouseY - grabbeddata.y
+                x, y = x + xdiff, y + ydiff
+                grabbeddata.x, grabbeddata.y = mouseX, mouseY
+                for i = 2, #elements, 1 do
+                    local elementX, elementY = elements[i].getPos()
+                    elements[i].changePos(elementX + xdiff, elementY + ydiff)
+                end
+            end
+        end
+    }
+end
+
 local frame = {
     ---@param elements table
     ---@return frame
-    new = function(elements)
+    new = function(elements, x, y, w, h, title, bgclr, fgclr)
+        table.insert(elements, 1, newFrameObj(x, y, w, h, bgclr, fgclr, title, elements))
         ---@class frame
         local frame = {
             elements = elements,
--- testing/main.lua
+++ testing/main.lua
@@ -6,37 +6,30 @@ local tween = require("libs.guified.modules.experimental.tween")
 local logger = guified.debug.logger
 logger.info("Hello World !")
 
-local movbox = guified.registry.elements.box:new(0, 0, 40, 40, "line", colors.electric_blue)
-local mouseLoc = guified.registry.elements.text:new(love.mouse.getX(), love.mouse.getY(), tostring(love.mouse.getX().."\n"..tostring(love.mouse.getY())))
-guified.registry.register(mouseLoc)
+--local movbox = guified.registry.elements.box:new(0, 0, 40, 40, "line", colors.electric_blue)
+--local mouseLoc = guified.registry.elements.text:new(love.mouse.getX(), love.mouse.getY(), tostring(love.mouse.getX().."\n"..tostring(love.mouse.getY())))
+--guified.registry.register(mouseLoc)
 local frame = framemod.new({
-    guified.registry.elements.text:new(0, 0, guified.__VER__),
-    guified.registry.elements.text:new(love.graphics.getWidth() / 2, 0, "Hi !"),
-    guified.registry.elements.box:new(0, 1, love.graphics.getWidth() - 1, love.graphics.getHeight() - 1, "line", colors.cyan),
-    movbox,
-    guified.registry.elements.textInput:new(100, 100, 40, 40)
-})
+    guified.registry.elements.text:new(0, 30, "This is Guified "..guified.__VER__),
+    guified.registry.elements.text:new(0, 15, "Hi !"),
+    --guified.registry.elements.box:new(0, 1, love.graphics.getWidth() - 1, love.graphics.getHeight() - 1, "line", colors.cyan),
+    --movbox,
+    --guified.registry.elements.textInput:new(100, 100, 40, 40)
+}, 0, 0, 150, 60, "Guified")
 function love.load()
     print("Using guified version "..guified.__VER__)
     --guified.funcs.setWindowToBeOnTop()
     frame:load()
     --print(frame:addSlider(30, 30).id)
     local box = guified.registry.elements.box:new(0, 0, 40, 40, "fill", colors.green)
-    local tweenO = tween.newElementTween(box, 0, 0, love.graphics.getWidth() - 40, love.graphics.getHeight() / 2)
-    guified.registry.register(tweenO)
-    tweenO.exec()
-    local button = guified.registry.elements.button:new(love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, "Hello World !")
-    guified.registry.register(button)
-    local txtinput2 = guified.registry.elements.textInput:new(100, 300, 40, 40)
-    guified.registry.register(txtinput2)
-    guified.registry.register({
-        name = "error causer"
-    })
+    --local tweenO = tween.newElementTween(box, 0, 0, love.graphics.getWidth() - 40, love.graphics.getHeight() / 2)
+    --guified.registry.register(tweenO)
+    --tweenO.exec()
 end
 function love.update(dt)
-    movbox.changeSize(love.mouse.getX(), love.mouse.getY())
-    mouseLoc.changePos(love.mouse.getX() + 10, love.mouse.getY())
-    mouseLoc.text("X:"..tostring(love.mouse.getX().."\nY:"..tostring(love.mouse.getY())))
+    --movbox.changeSize(love.mouse.getX(), love.mouse.getY())
+    --mouseLoc.changePos(love.mouse.getX() + 10, love.mouse.getY())
+    --mouseLoc.text("X:"..tostring(love.mouse.getX().."\nY:"..tostring(love.mouse.getY())))
     if love.keyboard.isDown("e") then
         error("TEST")
     end
@@ -44,7 +37,6 @@ end
 function love.keypressed(key)
     if key == "u" then
         frame:unload()
-        guified.registry.remove(mouseLoc)
         guified.debug.warn("FRAME UNLOADED")
     end
 end
