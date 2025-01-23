e60cbe6cf86cbdec43a86538c68bf104467560e4
zalanwastaken <muditmishra.mr@gmail.com>
2025-01-23 20:55:07 +0530

    new module

---
+++ testing/libs/guified/modules/shotty.lua
@@ -0,0 +1,11 @@
+---@type guified
+local guified = require(__GUIFIEDGLOBAL__.rootfolder.."init")
+---@class shotty
+local shotty = {
+    elements = guified.registry.elements,
+    register = guified.registry.register,
+    remove = guified.registry.remove,
+    extcalls = guified.extcalls,
+    logger = guified.debug.logger
+}
+return(shotty)
