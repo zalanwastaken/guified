082ad5d25448378e8568a6d71eb63d0b685c0e59
zalanwastaken <muditmishra.mr@gmail.com>
2025-04-14 13:44:02 +0530

    added errorhandeling and comments

--- src/testing/libs/guified/elements.lua
+++ src/testing/libs/guified/elements.lua
@@ -1,11 +1,42 @@
+local elementsinternal = {
+    funcs = {
+        checkArg = function(arg, argnum, expected, name)
+            if type(arg):lower() ~= expected then
+                error("Argument #"..argnum.." to "..name.." expected "..expected.." got "..type(arg))
+            end
+        end
+    },
+    types = {
+        string = "string",
+        number = "number",
+        int = "number",
+        dict = "table",
+        table = "table",
+        null = "nil",
+        nil_val = "nil"
+    }
+}
+
 ---@class elements
 local elements = {
+    ---@param text string
+    ---@param x number
+    ---@param y number
+    ---@param w number Optional
+    ---@param h number Optional
+    ---@param fgclr table Optional
+    ---@param bgclr table Optional
+    ---@param activebtn number Optional
     button = function(text, x, y, w, h, bgclr, fgclr, activebtn)
         bgclr = bgclr or {1, 1, 1, 1}
         fgclr = fgclr or {0, 0, 0, 1}
         w = w or #text * __GUIFIEDGLOBAL__.fontsize
         h = h or __GUIFIEDGLOBAL__.fontsize*2
         activebtn = activebtn or 1
+
+        elementsinternal.funcs.checkArg(text, 1, elementsinternal.types.string, "button")
+        elementsinternal.funcs.checkArg(x, 2, elementsinternal.types.int, "button")
+        elementsinternal.funcs.checkArg(y, 3, elementsinternal.types.int, "button")
 
         local isPressed = false
 
@@ -49,9 +80,15 @@ local elements = {
         })
     end,
 
+    ---@param text string
+    ---@param x number Optional
+    ---@param y number Optional
     text = function(text, x, y)
         x = x or 0
         y = y or 0
+
+        elementsinternal.funcs.checkArg(text, 1, elementsinternal.types.string, "text")
+
         return({
             name = "text: "..text,
             draw = function()
@@ -60,8 +97,19 @@ local elements = {
         })
     end,
 
+    ---@param text string
+    ---@param x number Optional
+    ---@param y number Optional
+    ---@param align string Optional
+    ---@param maxalign number Optional
     textf = function(text, x, y, align, maxalign)
         maxalign = maxalign or love.graphics.getWidth()
+        x = x or 0
+        y = y or 0
+        align = align or "center"
+
+        elementsinternal.funcs.checkArg(text, 1, elementsinternal.types.string, "textf")
+
         return({
             name = "textf: "..text,
             draw = function()
@@ -70,7 +118,7 @@ local elements = {
         })
     end,
 
-    guifiedsplash = function(self)
+    guifiedsplash = function()
         local largefont = love.graphics.newFont(20)
         local stdfont = __GUIFIEDGLOBAL__.font
         local quotes = {"Meow", "ZWT", "The CPU is a rock", "Lua > JS = true. Lua < JS = true. JS logic",
@@ -79,7 +127,7 @@ local elements = {
         local quote = quotes[love.math.random(1, #quotes)]
         local done = false
         return ({
-            name = "splash",
+            name = "splash element",
             draw = function()
                 love.graphics.setColor(0, 0, 0, alpha or 0)
                 love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
