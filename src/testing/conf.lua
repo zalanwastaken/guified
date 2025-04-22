2b1fe6919a5bb002b6f7cd359261cd55304dccbb
zalanwastaken <muditmishra.mr@gmail.com>
2025-04-22 19:20:41 +0530

    tween and image test

--- src/testing/conf.lua
+++ src/testing/conf.lua
@@ -2,5 +2,4 @@ function love.conf(t)
     t.console = true
     t.window.resizable = true
     t.window.vsync = false
-    --t.window.title = "Guified tests"
 end
--- src/testing/main.lua
+++ src/testing/main.lua
@@ -1,10 +1,32 @@
-local guified = require("libs.guified")
+love.window.setTitle("disable hook meow") --? to disable guified window title hook
+
+local guified = require("libs.guified.init")
 local logger = guified.debug.logger
+local tween = require("libs.guified.modules.tween")
 
-guified.elements.button()
+guified.registry.register(guified.elements.image(0, 0, "assets/name.png"))
 
-guified.registry.register(guified.elements.button("Hello world !", love.graphics.getWidth() / 2, love.graphics.getHeight() / 2))
+print(tween)
 
-guified.registry.register(guified.elements.text("hello world !"))
+local fpsx, fpsy = 0, 0
 
-guified.registry.register(guified.elements.textf("Hello", 0, 0, "center"))
+local fps_counter = {
+    name = "FPS",
+    draw = function()
+        love.graphics.setColor(1, 0, 0)
+        love.graphics.print(love.timer.getFPS(), fpsx, fpsy)
+    end,
+    setPOS = function(argx, argy)
+        fpsx = argx
+        fpsy = argy
+    end,
+    getPOS = function()
+        return fpsx, fpsy
+    end
+}
+
+local fps_tween = tween.newElementTween(fps_counter, love.math.random(0, love.graphics.getWidth()), love.math.random(0, love.graphics.getHeight()), 10)
+
+guified.registry.register(fps_tween)
+
+fps_tween.start()
