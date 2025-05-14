df84063b56cb3cdbd2368f4d29fee9c9c8ce6fb8
zalanwastaken <muditmishra.mr@gmail.com>
2025-05-14 18:48:15 +0530

    rewrite started

--- src/testing/libs/guified/modules/experimental/grid.lua
+++ src/testing/libs/guified/modules/experimental/grid.lua
@@ -2,68 +2,26 @@ if __GUIFIEDGLOBAL__ == nil then
     return nil
 end
 
----@type guified
-local guified = require(__GUIFIEDGLOBAL__.rootfolder..".init")
-local logger = guified.debug.logger
+local gridInternal = {
+    funcs = require(__GUIFIEDGLOBAL__.rootfolder .. ".dependencies.internal.funcs"),
+}
 
 local grid = {
-    newGrid = function(sx, sy, ex, ey)
-        local elements = {}
-        local lastpos = {
-            x = sx,
-            y = sy
-        } --? x, y
-        local enabled = false
-        return({
-            ---@param element element
-            ---@param paddingx number
-            ---@param paddingy number
-            register = function(element, paddingx, paddingy)
-                element.changePos(lastpos.x, lastpos.y)
-                lastpos.x = lastpos.x + paddingx
-                lastpos.y = lastpos.y + paddingy
-                elements[#elements + 1] = element
-            end,
-
-            ---@param elementno number
-            remove = function(elementno)
-                if not(enabled) then
-                    table.remove(elements, elementno)
-                else
-                    logger.error("Grid needs to be disabled to be able to remove elements")
+    newGrid = function(sx, sy, ex, ey, idlen)
+        local elemnts = {}
+        local nextX = sx
+        local nextY = sy
+        return ({
+            sx = sx,
+            sy = sy,
+            ex = ex,
+            ey = ey,
+            addElement = function(element, w, h, padding )
+                if not(element.getPOS and element.setPOS) then
+                    return nil
                 end
-            end,
-
-            ---@param x number
-            ---@param y number
-            setlastpos = function(x, y)
-                lastpos.x = x
-                lastpos.y = y
-            end,
-
-            enable = function()
-                if not(enabled) then
-                    enabled = true
-                    for i = 1, #elements, 1 do
-                        guified.registry.register(elements[i])
-                    end
-                end
-            end,
-
-            disable = function()
-                if enabled then
-                    enabled = false
-                    for i = 1, #elements, 1 do
-                        guified.registry.remove(elements[i])
-                    end
-                end
-            end,
-
-            toggle = function()
-                enabled = not(enabled)
+                
             end
         })
     end
 }
-
-return grid
