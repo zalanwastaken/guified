ff0a7a7085945a667541287e83be4f252fe7738b
zalanwastaken <muditmishra.mr@gmail.com>
2025-05-24 18:11:07 +0530

    updated

--- src/testing/libs/guified/modules/reactor.lua
+++ src/testing/libs/guified/modules/reactor.lua
@@ -1,4 +1,4 @@
---! REACTOR IS NOT TO BE CONFUSED WITH REACT THEY ARE COMPLETELY DIFFERENT THINGS
+-- ! REACTOR IS NOT TO BE CONFUSED WITH REACT THEY ARE COMPLETELY DIFFERENT THINGS
 --[[
 *░▒▓███████▓▒░░▒▓████████▓▒░░▒▓██████▓▒░ ░▒▓██████▓▒░▒▓████████▓▒░▒▓██████▓▒░░▒▓███████▓▒░  
 *░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
@@ -8,24 +8,22 @@
 *░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ ░▒▓█▓▒░  ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
 *░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░  ░▒▓█▓▒░   ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░ 
 ? Welcome to a dirty system capable of reaching 900 FPS
---]]
-
-if __GUIFIEDGLOBAL__ == nil then
+--]] if __GUIFIEDGLOBAL__ == nil then
     return nil
 end
 
 ---@type guified
-local guified = require(__GUIFIEDGLOBAL__.rootfolder..".init")
+local guified = require(__GUIFIEDGLOBAL__.rootfolder .. ".init")
 local logger = guified.debug.logger
 ---@type funcs
-local funcs = require(__GUIFIEDGLOBAL__.rootfolder..".dependencies.internal.funcs")
+local funcs = require(__GUIFIEDGLOBAL__.rootfolder .. ".dependencies.internal.funcs")
 
 logger.info("Reactor init started")
 
 local reactorinternal = {
     funcs = {
         draw = function(dirty)
-            if not(dirty) then
+            if not (dirty) then
                 return nil
             end
             love.graphics.origin()
@@ -36,7 +34,20 @@ local reactorinternal = {
             end
             -- * guified code end
             love.graphics.present()
+        end,
+        merge = function(t1, t2)
+            local result = {}
+            for k, v in pairs(t1) do
+                result[k] = v
+            end
+            for k, v in pairs(t2) do
+                result[k] = v
+            end
+            return result
         end
+    },
+    internalregistry = {
+        IMPL = {}
     },
     dirty = false
 }
@@ -56,19 +67,40 @@ local reactor = {
                     return elementIMPL[key]
                 end
             }
-            element = setmetatable({update = elementIMPL.update or nil, draw = elementIMPL.draw or nil}, mt)
+            element = setmetatable({
+                update = elementIMPL.update or nil,
+                draw = elementIMPL.draw or nil
+            }, reactorinternal.funcs.merge(getmetatable(element) or {}, mt))
             guified.registry.register(element, idlen)
+            reactorinternal.internalregistry.IMPL[element.id] = elementIMPL
             reactorinternal.dirty = true
-            return(element)
+            return (element)
+        end,
+        ---@param element element
+        remove = function(element)
+            local ret = reactorinternal.internalregistry.IMPL[element.id]
+            reactorinternal.internalregistry.IMPL[element.id] = nil
+            guified.registry.remove(element)
+            reactorinternal.dirty = true
+            return (ret)
         end
     },
     settings = {
-        --* works with most elements
+        -- * works with most elements
         default = {
             setPOS = true,
             setWH = true,
             setText = true
-        }
+        },
+        null = {}, -- ? bascially no settings
+        ---@param setname table a array to gen settings for
+        genSettings = function(setname)
+            local ret = {}
+            for i = 1, #setname, 1 do
+                ret[setname[i]] = true
+            end
+            return ret
+        end
     },
     funcs = {
         ---@param dirty boolean
@@ -118,7 +150,7 @@ function love.run()
         end -- will pass 0 if love.timer is disabled
         if love.graphics and love.graphics.isActive() then
             reactorinternal.funcs.draw(reactorinternal.dirty)
-            reactorinternal.dirty = reactorinternal.dirty and not(reactorinternal.dirty)
+            reactorinternal.dirty = reactorinternal.dirty and not (reactorinternal.dirty)
         end
         if love.timer then
             love.timer.sleep(0.001)
