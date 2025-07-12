4d7299cc89af546c0aa1ae389432898474d0b90a
zalanwastaken <muditmishra.mr@gmail.com>
2025-07-12 21:43:37 +0530

    B-2.0.2

--- src/testing2/main.lua
+++ src/testing2/main.lua
@@ -1,2 +1,2 @@
 local guified = require("libs.guified.init")
-guified.registry.register(guified.elements.text("Hello World !"))
+guified.registry.register(guified.elements.text("Hello World !", love.graphics.getWidth() / 2, love.graphics.getHeight() / 2))
